5683be14157fc251be9650a9f08ee7d969878707
XY56L7 <palmartin99@gmail.com>
2024-11-22 16:40:57 +0100

    Initialize the introduction component

---
+++ Szakdolgozat_Frontend_Angular/szakdolgozat-frontend/src/app/introduction/introduction.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-introduction',
+  standalone: true,
+  imports: [],
+  templateUrl: './introduction.component.html',
+  styleUrl: './introduction.component.css'
+})
+export class IntroductionComponent {
+
+}
