a0aa1451cd17c53619515e0f7b2084ecb4b1110b
XY56L7 <palmartin99@gmail.com>
2024-11-24 12:33:24 +0100

    Change the style of the buttons

--- Szakdolgozat_Frontend_Angular/szakdolgozat-frontend/src/app/introduction/introduction.component.ts
+++ Szakdolgozat_Frontend_Angular/szakdolgozat-frontend/src/app/introduction/introduction.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-introduction',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './introduction.component.html',
   styleUrl: './introduction.component.css'
 })
