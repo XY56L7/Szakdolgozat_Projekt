48f279131bc55b3d63ad9c3aebf26c0ebd406890
XY56L7 <palmartin99@gmail.com>
2024-11-29 20:58:22 +0100

    Get rid of the unnecessary lines

--- Szakdolgozat_Backend_Django/django_backend/api/urls.py
+++ Szakdolgozat_Backend_Django/django_backend/api/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.urls import path
-from .views import get_energy_data, evaluate_model, predict_energy ,register, login,get_users
+from .views import  evaluate_model, predict_energy ,register, login
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
@@ -10,10 +10,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('predict_energy/', predict_energy, name='predict_energy'),
-    path('users/energy/', get_energy_data ,name='get_energy_data'),
     path('register/', register, name='register'),
     path('login/', login, name='login'),
-    path('users/', get_users, name='get_users'),
     path('predict/', evaluate_model, name='predict_and_plot'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
--- Szakdolgozat_Backend_Django/django_backend/api/views.py
+++ Szakdolgozat_Backend_Django/django_backend/api/views.py
@@ -24,163 +24,13 @@ import matplotlib
 from asgiref.sync import sync_to_async
 matplotlib.use('Agg')
 
-
-def encode_season(season):
-    seasons = ["Winter", "Spring", "Summer", "Autumn"]
-    return [1 if s == season else 0 for s in seasons]
-
-def encode_category(category):
-    categories = ["kertes ház", "ikerház", "panel"]
-    return [1 if c == category else 0 for c in categories]
-@csrf_exempt
-def predict_energy(request):
-    if request.method == 'POST':
-        try:
-            data = json.loads(request.body)
-            year = data.get('year')
-            month = data.get('month')
-            day = data.get('day')
-            hour = data.get('hour')
-            number_of_panels = data.get('number_of_panels')
-            season = data.get('season')  
-            category = data.get('category') 
-
-            season_encoded = encode_season(season)
-            category_encoded = encode_category(category)
-
-            features = [
-                year, month, day, hour, number_of_panels, *season_encoded, *category_encoded
-            ]
-
-            # Előrejelzések
-            production = predict_production(features)
-            consumption = predict_consumption(features)
-
-            # Plotolás
-            fig, ax = plt.subplots(figsize=(10, 5))
-            ax.plot(['Production', 'Consumption'], [production, consumption], marker='o', label='Predictions')
-            ax.set_title('Energy Predictions')
-            ax.set_ylabel('Power (kW)')
-            ax.set_xlabel('Category')
-            ax.legend()
-            ax.grid(True)
-
-            # Fájl mentése
-            random_number = random.randint(0, 10000)
-            image_filename = f'energy_predictions_{random_number}.png'
-            image_path = os.path.join(MEDIA_DIR, image_filename)
-            plt.savefig(image_path)
-            plt.close(fig)
-
-            # Ellenőrzés
-            if os.path.exists(image_path):
-                print(f"Image saved at {image_path}")
-            else:
-                print("Image not found")
-
-            return JsonResponse({
-                'production_power': production,
-                'consumption_power': consumption,
-                'plot_url': request.build_absolute_uri('/api' + settings.MEDIA_URL + image_filename)
-            })
-
-        except Exception as e:
-            return JsonResponse({'error': str(e)}, status=400)
-
-    return JsonResponse({'error': 'Invalid request'}, status=400)
-
-# @csrf_exempt
-# def predict_energy(request):
-#     if request.method == 'POST':
-#         try:
-#             data = json.loads(request.body)
-#             year = data.get('year')
-#             month = data.get('month')
-#             day = data.get('day')
-#             hour = data.get('hour')
-#             number_of_panels = data.get('number_of_panels')
-#             season = data.get('season')  
-#             category = data.get('category') 
-
-#             season_encoded = encode_season(season)
-#             category_encoded = encode_category(category)
-
-#             features = [
-#                 year, month, day, hour, number_of_panels, *season_encoded, *category_encoded
-#             ]
-
-#             print(features)
-
-#             production = predict_production(features)
-#             consumption = predict_consumption(features)
-
-#             return JsonResponse({'production_power': production, 'consumption_power': consumption})
-
-#         except Exception as e:
-#             return JsonResponse({'error': str(e)}, status=400)
-
-#     return JsonResponse({'error': 'Invalid request'}, status=400)
-
-
-def predict_production(features):
-    model_path = os.path.join(settings.BASE_DIR, 'api', 'models', 'production_model.keras')
-    scaler_path = os.path.join(settings.BASE_DIR, 'api', 'models', 'scaler.pkl')
-    model = load_model(model_path)
-    scaler = joblib.load(scaler_path)
-
-    features_scaled = scaler.transform([features])
-
-    prediction = model.predict(features_scaled)
-    return float(prediction[0][0])
-
-def predict_consumption(features):
-    model_path = os.path.join(settings.BASE_DIR, 'api', 'models', 'consumption_model.keras')
-    scaler_path = os.path.join(settings.BASE_DIR, 'api', 'models', 'scaler.pkl')
-    model = load_model(model_path)
-    scaler = joblib.load(scaler_path)
-
-    features_scaled = scaler.transform([features])
-    
-    prediction = model.predict(features_scaled)
-    return float(prediction[0][0])
-
-
 BASE_DIR = settings.BASE_DIR 
 MEDIA_DIR = os.path.join(BASE_DIR, 'api', 'media')
-
-def predict_power_consumption(v_rms, i_rms, s,device):
-    MODEL_PATH = os.path.join(BASE_DIR, 'api', 'models', f'{device}',f'{device}.keras')
-
-    INPUT_SCALER = os.path.join(BASE_DIR, 'api', 'models', f'{device}',f'{device}_scaler_features.pkl')
-    OUTPUT_SCALER = os.path.join(BASE_DIR, 'api', 'models', f'{device}',f'{device}_scaler_target.pkl')
-    model = load_model(MODEL_PATH)
-
-    input_scaler = joblib.load(INPUT_SCALER)
-    output_scaler = joblib.load(OUTPUT_SCALER)
-
-    input_data = pd.DataFrame([[v_rms, i_rms, s]], columns=['V_rms', 'I_rms', 'S'])
-    
-    input_data_scaled = input_scaler.transform(input_data)
-    input_data_scaled = np.reshape(input_data_scaled, (1, 1, 3))
-
-    prediction = model.predict(input_data_scaled)
-
-    print(f'Prediction: {prediction}')
-    
-    predicted_power_scaled = prediction[0, 0]  
-
-    predicted_power = output_scaler.inverse_transform([[predicted_power_scaled]])[0][0]
-
-    return abs(predicted_power)
-
-
 
 @csrf_exempt
 @require_http_methods(["POST"])
 async def evaluate_model(request):
-    try:
-        print("Raw request body:", request.body)
-        
+    try:        
         data = json.loads(request.body)
         print("Parsed data:", data)
         
@@ -191,10 +41,8 @@ async def evaluate_model(request):
         i_rms = float(data.get('I_rms'))
         s = float(data.get('S'))
         device = str(data.get('Device'))
-        print(f"Extracted values - V_rms: {v_rms}, I_rms: {i_rms}, S: {s}")
 
         predicted_power = await sync_to_async(predict_power_consumption)(v_rms, i_rms, s,device)
-        print("Predicted power:", predicted_power)
 
         fig, ax = plt.subplots()
         ax.plot([v_rms, i_rms, s], label='Input Data')
@@ -208,15 +56,9 @@ async def evaluate_model(request):
         image_path = os.path.join(MEDIA_DIR, image_filename)
 
         image_path = os.path.normpath(image_path)
-        print(f"image_path: {image_path}")
 
         plt.savefig(image_path)
         plt.close(fig)
-
-        if os.path.exists(image_path):
-            print("Image exists:", image_path)
-        else:
-            print("Image does not exist.")
 
         response_data = {
             'image_path': request.build_absolute_uri('/api' + settings.MEDIA_URL + image_filename),
@@ -232,15 +74,8 @@ async def evaluate_model(request):
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=400)
 
-@api_view(['GET'])
-def get_users(request):
-    users = User.objects.all() 
-    serializer = UserSerializer(users, many=True) 
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
 @api_view(['POST'])
 def register(request):
-    print("Line 9 Register method in views.py")
     serializer = RegisterSerializer(data=request.data)
     if serializer.is_valid():
         user = serializer.save()
@@ -266,23 +101,103 @@ def login(request):
         })
     return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
-@api_view(['GET'])
-def get_energy_data(request):
-        # Fake data
-        fake_data = {
-            "device_option": "option1",
-            "devices": ["device1", "device2", "device3"],
-            "time_interval": "1h",
-            "prediction_model": "basic_model",
-            "V_rms": 220.5,
-            "I_rms": 10.2,
-            "P": 2235.1,
-            "S": 2300.0,
-            "start_date": "2024-01-01",
-            "end_date": "2024-01-31",
-            "custom_model_file": None  
-        }
+@csrf_exempt
+def predict_energy(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            year = data.get('year')
+            month = data.get('month')
+            day = data.get('day')
+            hour = data.get('hour')
+            number_of_panels = data.get('number_of_panels')
+            season = data.get('season')  
+            category = data.get('category') 
 
-        serializer = EnergyAnalysisSerializer(fake_data)
+            season_encoded = encode_season(season)
+            category_encoded = encode_category(category)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+            features = [
+                year, month, day, hour, number_of_panels, *season_encoded, *category_encoded
+            ]
+
+            production = predict_production(features)
+            consumption = predict_consumption(features)
+
+            fig, ax = plt.subplots(figsize=(10, 5))
+            ax.plot(['Production', 'Consumption'], [production, consumption], marker='o', label='Predictions')
+            ax.set_title('Energy Predictions')
+            ax.set_ylabel('Power (kW)')
+            ax.set_xlabel('Category')
+            ax.legend()
+            ax.grid(True)
+
+            random_number = random.randint(0, 10000)
+            image_filename = f'energy_predictions_{random_number}.png'
+            image_path = os.path.join(MEDIA_DIR, image_filename)
+            plt.savefig(image_path)
+            plt.close(fig)
+
+            return JsonResponse({
+                'production_power': production,
+                'consumption_power': consumption,
+                'plot_url': request.build_absolute_uri('/api' + settings.MEDIA_URL + image_filename)
+            })
+
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
+
+    return JsonResponse({'error': 'Invalid request'}, status=400)
+
+def predict_production(features):
+    model_path = os.path.join(settings.BASE_DIR, 'api', 'models', 'production_model.keras')
+    scaler_path = os.path.join(settings.BASE_DIR, 'api', 'models', 'scaler.pkl')
+    model = load_model(model_path)
+    scaler = joblib.load(scaler_path)
+
+    features_scaled = scaler.transform([features])
+
+    prediction = model.predict(features_scaled)
+    return float(prediction[0][0])
+
+def predict_consumption(features):
+    model_path = os.path.join(settings.BASE_DIR, 'api', 'models', 'consumption_model.keras')
+    scaler_path = os.path.join(settings.BASE_DIR, 'api', 'models', 'scaler.pkl')
+    model = load_model(model_path)
+    scaler = joblib.load(scaler_path)
+
+    features_scaled = scaler.transform([features])
+    
+    prediction = model.predict(features_scaled)
+    return float(prediction[0][0])
+
+def predict_power_consumption(v_rms, i_rms, s,device):
+    MODEL_PATH = os.path.join(BASE_DIR, 'api', 'models', f'{device}',f'{device}.keras')
+
+    INPUT_SCALER = os.path.join(BASE_DIR, 'api', 'models', f'{device}',f'{device}_scaler_features.pkl')
+    OUTPUT_SCALER = os.path.join(BASE_DIR, 'api', 'models', f'{device}',f'{device}_scaler_target.pkl')
+    model = load_model(MODEL_PATH)
+
+    input_scaler = joblib.load(INPUT_SCALER)
+    output_scaler = joblib.load(OUTPUT_SCALER)
+
+    input_data = pd.DataFrame([[v_rms, i_rms, s]], columns=['V_rms', 'I_rms', 'S'])
+    
+    input_data_scaled = input_scaler.transform(input_data)
+    input_data_scaled = np.reshape(input_data_scaled, (1, 1, 3))
+
+    prediction = model.predict(input_data_scaled)
+    
+    predicted_power_scaled = prediction[0, 0]  
+
+    predicted_power = output_scaler.inverse_transform([[predicted_power_scaled]])[0][0]
+
+    return abs(predicted_power)
+
+def encode_season(season):
+    seasons = ["Winter", "Spring", "Summer", "Autumn"]
+    return [1 if s == season else 0 for s in seasons]
+
+def encode_category(category):
+    categories = ["kertes ház", "ikerház", "panel"]
+    return [1 if c == category else 0 for c in categories]
