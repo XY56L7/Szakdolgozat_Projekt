9ddd50309c710ff35adaf0ffe9cbe1819c8050c2
Biró Tamás <biro.tamas@stud.uni-obuda.hu>
2025-12-14 22:36:48 +0100

    added new selection for another model

--- Szakdolgozat_Frontend_Angular/szakdolgozat-frontend/src/app/communities/communities.component.ts
+++ Szakdolgozat_Frontend_Angular/szakdolgozat-frontend/src/app/communities/communities.component.ts
@@ -19,36 +19,49 @@ export class CommunitiesComponent {
   };
   predictedP: number | undefined;
   plotUrl: string | undefined;
+  selectedModel: string = '';
+  selectedFile: File | null = null;
 
   s_options = ['Winter', 'Spring', 'Summer', 'Autumn'];
   c_options = ['kertes ház', 'ikerház', 'panel lakás', 'apartman', 'sorház', 'tanya', 'kunyhó', 'családi ház', 'irodaház', 'iskola', 'hivatal', 'könyvtár', 'gyár']
 
-  constructor(private communitiesService: communitiesService, private router: Router) {}
+  constructor(private communitiesService: communitiesService, private router: Router) { }
+
+  onFileSelected(event: any) {
+    if (event.target.files.length > 0) {
+      this,this.selectedFile = event.target.files[0];
+    }
+  }
 
   predict(){
-    const newCommunitiesData: CommunitiesData = {
-      timestamp: this.formData.timestamp,
-      number_of_panels: this.formData.number_of_panels,
-      panel_area_m2: this.formData.panel_area_m2,
-      category: this.formData.category,
-      consumption: this.formData.consumption,
-      air_temp: this.formData.air_temp,
-      clearsky_dhi: this.formData.clearsky_dhi,
-      clearsky_dni: this.formData.clearsky_dni,
-      clearsky_ghi: this.formData.clearsky_ghi,
-      clearsky_gti: this.formData.clearsky_gti,
-      cloud_opacity: this.formData.cloud_opacity,
-      dhi: this.formData.dhi,
-      dni: this.formData.dni,
-      ghi: this.formData.ghi,
-      gti: this.formData.gti,
-      season: this.formData.season,
-      snow_soiling_ground: this.formData.snow_soiling_ground,
-      snow_soiling_rooftop: this.formData.snow_soiling_rooftop
-    }
+    if (this.selectedModel == "Reg") {
+      const newCommunitiesData: CommunitiesData = {
+        timestamp: this.formData.timestamp,
+        number_of_panels: this.formData.number_of_panels,
+        panel_area_m2: this.formData.panel_area_m2,
+        category: this.formData.category,
+        consumption: this.formData.consumption,
+        air_temp: this.formData.air_temp,
+        clearsky_dhi: this.formData.clearsky_dhi,
+        clearsky_dni: this.formData.clearsky_dni,
+        clearsky_ghi: this.formData.clearsky_ghi,
+        clearsky_gti: this.formData.clearsky_gti,
+        cloud_opacity: this.formData.cloud_opacity,
+        dhi: this.formData.dhi,
+        dni: this.formData.dni,
+        ghi: this.formData.ghi,
+        gti: this.formData.gti,
+        season: this.formData.season,
+        snow_soiling_ground: this.formData.snow_soiling_ground,
+        snow_soiling_rooftop: this.formData.snow_soiling_rooftop
+      }
 
-    this.communitiesService.getPrediction(newCommunitiesData).subscribe(response => {
-      this.predictedP = response.predicted_production;
-    })
+      this.communitiesService.getPrediction(newCommunitiesData).subscribe(response => {
+        this.predictedP = response.predicted_production;
+      })
+    }
+    else if (this.selectedModel == "GRU") {
+
+    }
   }
 }
